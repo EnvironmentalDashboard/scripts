03dc9c74f12ef67c414951ddcd042b03eb354a27
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2017-12-30 23:23:39 -0600

    now it works

--- resize_images.php
+++ resize_images.php
@@ -3,7 +3,7 @@ error_reporting(-1);
 ini_set('display_errors', 'On');
 chdir(__DIR__);
 require '../includes/db.php';
-foreach ($db->query('SELECT id, img FROM calendar WHERE img IS NOT NULL AND thumbnail IS NULL') as $event) {
+foreach ($db->query('SELECT id, img FROM calendar WHERE img IS NOT NULL') as $event) {
   $img = imagecreatefromstring($event['img']);
   if (!$img) {
     continue;
@@ -19,10 +19,17 @@ foreach ($db->query('SELECT id, img FROM calendar WHERE img IS NOT NULL AND thum
   $output = ob_get_clean();
   $fn = "tmp.{$ext}";
   file_put_contents($fn, $output);
-  shell_exec("convert {$fn} -define {$ext}:extent=64kb output.{$ext}"); // https://stackoverflow.com/a/11920384/2624391
-  $fp = fopen("output.{$ext}", 'rb');
-  $stmt = $db->prepare('UPDATE calendar SET thumbnail = ? WHERE id = ?');
-  $stmt->execute([$fp, $event['id']]);
+  shell_exec("convert {$fn} -define {$ext}:extent=52kb output.{$ext}"); // https://stackoverflow.com/a/11920384/2624391
+  if (filesize("output.{$ext}") < 65535) {
+    $fp = fopen("output.{$ext}", 'rb');
+    $stmt = $db->prepare('UPDATE calendar SET thumbnail = ? WHERE id = ?');
+    $stmt->bindParam(1, $fp, PDO::PARAM_LOB);
+    $stmt->bindParam(2, $event['id']);
+    $stmt->execute();
+  } else {
+    $stmt = $db->prepare('UPDATE calendar SET thumbnail = ? WHERE id = ?');
+    $stmt->execute([null, $event['id']]);
+  }
   unlink("output.{$ext}");
   unlink($fn);
 }
