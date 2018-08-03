d236520214ed35fd9925895d0c0e8a46f5d2c8be
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-08-03 17:19:58 -0400

    backup cv db

--- db_backup.sh
+++ db_backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 fn=`date -d "today" +"%Y_%m_%d_%H:%M"`
-`mysqldump -u root -pxvyb7g88 oberlin_environmentaldashboard > /root/db_backups/db_backup.sql 2> /dev/null`
-`mv /root/db_backups/db_backup.sql /root/db_backups/$fn.sql`
+`mysqldump -u root -pxvyb7g88 oberlin_environmentaldashboard > /root/db_backups/oberlin_environmentaldashboard/db_backup.sql 2> /dev/null`
+`mv /root/db_backups/oberlin_environmentaldashboard/db_backup.sql /root/db_backups/oberlin_environmentaldashboard/$fn.sql`
+`mysqldump -u root -pxvyb7g88 community_voices > /root/db_backups/community_voices/db_backup.sql 2> /dev/null`
+`mv /root/db_backups/community_voices/db_backup.sql /root/db_backups/community_voices/$fn.sql`
