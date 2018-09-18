98197a18183883b1807e144709dc47bb2c5d4c56
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-09-17 22:48:13 -0400

    modified slightly

--- backup.sh
+++ backup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 cd /root/
+apt-clone clone pckgs && rsync -azP /root/pckgs.apt-clone.tar.gz steve@ajlc.csr.oberlin.edu:/home/steve/backups/pckgs.apt-clone.tar.gz # https://unix.stackexchange.com/a/208163
+crontab -l > /root/crontab_backup && rsync -azP /root/crontab_backup steve@ajlc.csr.oberlin.edu:/home/steve/backups/crontab
 rsync -azP /var/www/uploads/ steve@ajlc.csr.oberlin.edu:/home/steve/backups/uploads
-rsync -azP /etc/apache2/sites-available/ steve@ajlc.csr.oberlin.edu:/home/steve/backups/server-config/sites-available
-rsync -azP /etc/ssl/ steve@ajlc.csr.oberlin.edu:/home/steve/backups/server-config/ssl
+rsync -azP /etc/ steve@ajlc.csr.oberlin.edu:/home/steve/backups/etc
 rsync -azP /root/db_backups/ steve@ajlc.csr.oberlin.edu:/home/steve/backups/db_backups
-crontab -l > /root/crontab_backup && rsync -azP /root/crontab_backup steve@ajlc.csr.oberlin.edu:/home/steve/backups/server-config/crontab
-apt-clone clone pckgs && rsync -azP /root/pckgs.apt-clone.tar.gz steve@ajlc.csr.oberlin.edu:/home/steve/backups/apt/pckgs.apt-clone.tar.gz # https://unix.stackexchange.com/a/208163
-rsync -azP /var/www/PHPMailer/ steve@ajlc.csr.oberlin.edu:/home/steve/backups/mail-server/PHPMailer/
-rsync -azP /etc/opendkim/ steve@ajlc.csr.oberlin.edu:/home/steve/backups/mail-server/opendkim/
+rsync -azP /var/www/PHPMailer/ steve@ajlc.csr.oberlin.edu:/home/steve/backups/PHPMailer
 rsync -azP /var/secret/ steve@ajlc.csr.oberlin.edu:/home/steve/backups/secret
