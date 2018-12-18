139660cebf2b4669bb08c0b587ebb2c71614b7d5
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-12-17 20:49:27 -0500

    changed user

--- restore_files.sh
+++ restore_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # this script is meant to find specific regular files (i.e. images) in a backup folder and move them to the server if they don't exist already
 # reads list of files to find from stdin; reads directory to move files to as first cli argument
-# usage: `cat list_of_files | ./restore_files.sh /var/www/uploads/CV_Media/images/`
+# usage: `cat list_of_files | ./restore_files.sh /var/www/uploads/CV_Media/images`
 # the above command will fetch each file specified in list_of_files (1 per line)
 # from the backup server (currently hardcoded) and place them in /var/www/uploads/CV_Media/images/
 # use the -f flag to overwrite files in target_dir if it already exists
@@ -13,7 +13,7 @@ do
 	lns=`echo "$res" | wc -l`
 	if [ $lns -eq 1 ]; then # only 1 file found
 		printf "$res => $1\n"
-		argumentArray=("$res" "root@159.89.232.129:$1")
+		argumentArray=("$res" "steve@159.89.232.129:$1")
 		scp "${argumentArray[@]}"
 	else
 		printf "Multiple files:\n$res"
