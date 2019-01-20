ff3e4adf01a7e1ae0f42efd86b7431d8f022848d
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-01-19 22:55:31 -0500

    also send email

--- sync_ssl.sh
+++ sync_ssl.sh
@@ -2,3 +2,4 @@
 
 scp -r /etc/letsencrypt/live nyc1@ajlc.csr.oberlin.edu:/etc/letsencrypt/
 scp /etc/letsencrypt/options-ssl-apache.conf nyc1@ajlc.csr.oberlin.edu:/etc/letsencrypt/
+echo "Updated SSL certificates" | mail -s "Server message" trobertf@oberlin.edu
