e206741a4f3bbea604980325d4109cb7c4d1beae
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-01-20 00:19:29 -0500

    small changes

--- sync_static_assets.sh
+++ sync_static_assets.sh
@@ -2,11 +2,9 @@
 
 fn="${BASH_SOURCE%/*}/ip.cache"
 # if ls says cache is missing curl it
-# if its not, find will return filename if ip.cache modification >1 day
-# grep for whitespace returned otherwise
-ls "$fn" > /dev/null 2>&1 && \
-find "$fn" -mmin +1440 2>/dev/null | grep -q '[^[:space:]]' \
-|| true
+# if its not, find will return filename if ip.cache modification >1 day, grep for whitespace returned otherwise
+ls "$fn" > /dev/null 2>&1 || true && \
+find "$fn" -mmin +1440 2>/dev/null | grep -q '[^[:space:]]'
 res=$?
 # ip.cache contains non-whitespace charachters ie file is older than 1 day
 if [ $res -eq 0 ]; then
@@ -14,7 +12,7 @@ if [ $res -eq 0 ]; then
 fi
 ip=`cat "$fn"`
 if [ "$ip" == "159.89.232.129" ]; then
-  rsync -azP /var/www/uploads nyc1@ajlc.csr.oberlin.edu:/var/www/
+  rsync -az /var/www/uploads nyc1@ajlc.csr.oberlin.edu:/var/www/
 elif [ "$ip" == "132.162.36.210" ]; then
-  rsync -azP /var/www/uploads ajlc-csr@159.89.232.129:/var/www/
+  rsync -az /var/www/uploads ajlc-csr@159.89.232.129:/var/www/
 fi
