9a8202461a92447f27fa5e8312f8a51ec3294d7c
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-02-11 18:16:10 -0500

    wrote script

--- gifduration/update_durations.php
+++ gifduration/update_durations.php
@@ -1,1 +1,9 @@
 <?php
+require '../../includes/db.php';
+foreach ($db->query("SELECT DISTINCT name FROM time_series WHERE length = 0") as $row) {
+  if (file_exists("/var/www/repos/chart/images/{$row['name']}.gif")) {
+    $res = intval(shell_exec("python ./gifduration-script/gifduration.py /var/www/repos/chart/images/{$row['name']}.gif"));
+    $stmt = $db->prepare('UPDATE time_series SET length = ? WHERE name = ?');
+    $stmt->execute([$res, $row['name']]);
+  }
+}
