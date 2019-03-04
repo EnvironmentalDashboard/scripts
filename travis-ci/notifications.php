9aac0d1db36a1cb9eb93630973056a146f2c2f76
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-03-03 22:41:51 -0500

    need to pull on main server & nodes

--- travis-ci/notifications.php
+++ travis-ci/notifications.php
@@ -8,7 +8,7 @@ if (in_array($_SERVER['REMOTE_ADDR'], ['54.173.229.200', '54.175.230.252'])) {
 		$payload = json_decode($_POST['payload']);
 		if ($payload['result'] === 0) {
 			if ($_GET['repo'] === 'community-voices') {
-				shell_exec("cd /var/www/repos/community-voices && git pull");
+				shell_exec("cd /var/www/repos/community-voices && git pull && ssh -t root@ajlc.csr.oberlin.edu \"cd /var/www/repos/community-voices && git pull\"");
 			} elseif ($_GET['repo'] === 'daemons') {
 				shell_exec('cd /var/repos/daemons && git pull && ' . dockerStopByImage('buildingosd') . ' && ./run.sh');
 			} elseif ($_GET['repo'] === 'relative-value') {
