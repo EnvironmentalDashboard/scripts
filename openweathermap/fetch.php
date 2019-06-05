4d5ead38cac45dc6e6bc6a640f71929d4e78d66c
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-06-05 14:54:56 -0400

    get weather for multiple locations

--- openweathermap/fetch.php
+++ openweathermap/fetch.php
@@ -1,23 +1,30 @@
 <?php
 require '/var/www/repos/includes/db.php';
 date_default_timezone_set('America/New_York');
-
-$key = 'aac6c14f6ed11c4787ed18ed20a5c18b';
-$lat = '41.29442793429477';
-$lon = '-82.21714374667339';
-$url = "https://api.openweathermap.org/data/2.5/weather?lat={$lat}&lon={$lon}&appid={$key}";
-$weather = json_decode(file_get_contents($url), true);
-$current_temp = $weather['main']['temp'];
 $time = time();
+$key = 'aac6c14f6ed11c4787ed18ed20a5c18b';
+$meters = [
+    'Oberlin Air Temperature' => ['lat' => '41.29442793429477', 'lon' => '-82.21714374667339'],
+    'Cleveland Air Temperature' => ['lat' => '41.29442793429477', 'lon' => '-82.21714374667339']
+];
 
-$meter_name = 'OpenWeatherMap air temperature';
-$stmt = $db->prepare('SELECT id FROM meters WHERE name = ? LIMIT 1');
-$stmt->execute([$meter_name]);
-$id = $stmt->fetchColumn();
-if ($id == false) {
-    $stmt = $db->prepare('INSERT INTO meters (building_id, source, scope, resource, name, calculated, units) VALUES (?, ?, ?, ?, ?, ?, ?)');
-    $stmt->execute([1, 'openweathermap', 'Other', 'Undefined', $meter_name, 1, 'Kelvin']);
-    $id = $db->lastInsertId();
+foreach ($meters as $meter => $coords) {
+    $url = "https://api.openweathermap.org/data/2.5/weather?lat={$coords['lat']}&lon={$coords['lon']}&appid={$key}";
+    $weather = json_decode(file_get_contents($url), true);
+    $current_temp = kelvin_to_fahrenheit($weather['main']['temp']);
+
+    $stmt = $db->prepare('SELECT id FROM meters WHERE name = ? LIMIT 1');
+    $stmt->execute([$meter]);
+    $id = $stmt->fetchColumn();
+    if ($id == false) {
+        $stmt = $db->prepare('INSERT INTO meters (building_id, source, scope, resource, name, calculated, units) VALUES (?, ?, ?, ?, ?, ?, ?)');
+        $stmt->execute([1, 'openweathermap', 'Other', 'Undefined', $meter, 1, 'Fahrenheit']);
+        $id = $db->lastInsertId();
+    }
+    $stmt = $db->prepare('REPLACE INTO meter_data (meter_id, value, recorded, resolution) VALUES (?, ?, ?, ?)');
+    $stmt->execute([$id, $current_temp, $time, 'hour']);
 }
-$stmt = $db->prepare('REPLACE INTO meter_data (meter_id, value, recorded, resolution) VALUES (?, ?, ?, ?)');
-$stmt->execute([$id, $current_temp, $time, 'hour']);
+
+function kelvin_to_fahrenheit($temp) {
+    return (($temp - 273.15) * 1.8) + 32;
+}
