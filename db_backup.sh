d405d7227b25e41d724eacc6521b39c802921f8d
Hitesh Jangid <hiteshjangid19@gmail.com>
2024-10-07 17:48:05 +0530

    update backup script, compress sql file into tar

--- db_backup.sh
+++ db_backup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-fn=`date -d "today" +"%Y_%m_%d_%H:%M"`
-mysqldump -ucron_tim -p93XWLYMfOiIcEvXo oberlin_environmentaldashboard > /root/db_backups/oberlin_environmentaldashboard/db_backup.sql 2> /dev/null
-mv /root/db_backups/oberlin_environmentaldashboard/db_backup.sql "/root/db_backups/oberlin_environmentaldashboard/$fn.sql"
-mysqldump -ucron_tim -p93XWLYMfOiIcEvXo community_voices > /root/db_backups/community_voices/db_backup.sql 2> /dev/null
-mv /root/db_backups/community_voices/db_backup.sql "/root/db_backups/community_voices/$fn.sql"
+# set the backup location
+cd /root/db_backups/
+
+fn=`date -d "today" +"%Y_%m_%d_%H_%M"`
+mysqldump -ucron_tim -p93XWLYMfOiIcEvXo oberlin_environmentaldashboard > oberlin_environmentaldashboard/db_backup.sql 2> /dev/null
+#compress the sql file into a tar 
+tar -czvf $fn.tar.gz oberlin_environmentaldashboard/db_backup.sql
+# remove the sql file once the compression is completed
+rm oberlin_environmentaldashboard/db_backup.sql
+
+#no need to export the community voices now
+# mysqldump -ucron_tim -p93XWLYMfOiIcEvXo community_voices > community_voices/db_backup.sql 2> /dev/null
+# tar -czvf $fn.tar.gz community_voices/db_backup.sql
+# remove the sql file once the compression is completed
+# rm community_voices/db_backup.sql
